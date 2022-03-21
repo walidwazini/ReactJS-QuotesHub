ffb2eb4a19e40becf77d3d4794ba55f6bf30db64
Walid Wazini <wazini25@gmail.com>
2022-03-22 07:56:19 +0800

    updates readme

--- src/Components/Comments/CommentList.js
+++ src/Components/Comments/CommentList.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { makeStyles } from '@mui/styles'
 
 const CommentList = ({ allComments }) => {
+  const classes = useStyles()
+
   return (
-    <div>CommentList</div>
+    <div>
+      <ul className={classes.commentsList}>
+        {allComments.map(comment => (
+          <li key={comment.id} className={classes.commentItem}>
+            <p>{comment.text}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
+const useStyles = makeStyles({
+  commentsList: {
+    listStyle: 'none',
+    margin: '2.5rem 0',
+    padding: 0,
+  },
+  commentItem: {
+    margin: '1rem 0',
+    fontSize: '1.24rem',
+    paddingBottom: '0.5rem',
+    color: 'white',
+    borderBottom: '2px solid grey'
+  }
+})
 export default CommentList
