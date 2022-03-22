132d193dbeb9b6c9c5382b94634bb408b02a0bc4
Walid Wazini <wazini25@gmail.com>
2022-03-23 05:32:12 +0800

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import { makeStyles } from "@mui/styles";
-import { Fragment, useState } from "react";
+import React, { Fragment, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Loading from "./Components/Loading";
 import MuiHeader from "./Components/MuiHeader";
-import AllQuotes from "./Pages/AllQuotes";
-import NewQuote from "./Pages/NewQuote";
-import QuoteDetail from "./Pages/QuoteDetail";
+
+// import AllQuotes from "./Pages/AllQuotes";
+// import NewQuote from "./Pages/NewQuote";
+// import QuoteDetail from "./Pages/QuoteDetail";
+
+const AllQuotes = React.lazy(() => import('./Pages/AllQuotes'))
+const NewQuote = React.lazy(() => import('./Pages/NewQuote'))
+const QuoteDetail = React.lazy(() => import('./Pages/QuoteDetail'))
 
 const App = () => {
   const classes = useStyles()
 
   return (
     <Fragment>
-      <MuiHeader />
-      <main className={classes.mainLayout} >
-        <Routes>
-          <Route path="/" element={<Navigate to='/quotes' />} />
-          <Route path="/quotes" element={<AllQuotes />} />
-          <Route path="/new-quote" element={<NewQuote />} />
-          <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Routes>
-      </main>
+      <Suspense fallback={
+        <div className="centered" >
+          <Loading />
+        </div>
+      } >
+        <MuiHeader />
+        <main className={classes.mainLayout} >
+          <Routes>
+            <Route path="/" element={<Navigate to='/quotes' />} />
+            <Route path="/quotes" element={<AllQuotes />} />
+            <Route path="/new-quote" element={<NewQuote />} />
+            <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Routes>
+        </main>
+      </Suspense>
     </Fragment>
   );
 }
